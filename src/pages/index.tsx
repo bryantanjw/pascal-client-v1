da8f10ee3eb6007c3698a3b85fde1f9d1d9b3678
TheClub4 <bryantan-888@hotmail.com>
2022-11-25 14:29:34 +0800

    Added mongodb cluster

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,40 @@
 import type { NextPage } from 'next'
+import { InferGetServerSidePropsType } from 'next'
 import Head from 'next/head'
-import styles from '@/styles/Home.module.css'
+import { Box, Stack, Heading, Text, useColorModeValue as mode } from '@chakra-ui/react'
+import clientPromise from '@/lib/mongodb'
+import Layout from 'components/Layout'
 import WithSubnavigation from 'components/TopBar'
 import MarketList from '@/components/Trade'
-import { Box, Stack, Heading, Text, useColorModeValue as mode, Image } from '@chakra-ui/react'
-import Layout from 'components/Layout'
 
-const Home: NextPage = () => {
+import styles from '@/styles/Home.module.css'
+
+export async function getServerSideProps(context) {
+  try {
+    await clientPromise
+    // `await clientPromise` will use the default database passed in the MONGODB_URI
+    // However you can use another database (e.g. myDatabase) by replacing the `await clientPromise` with the following code:
+    //
+    // `const client = await clientPromise`
+    // `const db = client.db("myDatabase")`
+    //
+    // Then you can execute queries against your database like so:
+    // db.find({}) or any of the MongoDB Node Driver commands
+
+    return {
+      props: { isConnected: true },
+    }
+  } catch (e) {
+    console.error(e)
+    return {
+      props: { isConnected: false },
+    }
+  }
+}
+
+const Home: NextPage = ({ isConnected }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  if (isConnected) console.log("Connected to MongoDB")
+  else(console.log("Not connected to MongoDB"))
   
   return (
     <div className={styles.container}>
