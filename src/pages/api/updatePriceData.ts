344147f5ec7f06fafca2b2c5d713c4478b461ed1
bryantanjw <bryantanjw01@gmail.com>
2023-01-22 08:36:21 +0800

    Testing updatePriceData api

--- src/pages/api/updatePriceData.ts
+++ src/pages/api/updatePriceData.ts
@@ -9,7 +9,41 @@ export default async function handler(
 ) {
   if (req.method === "POST") {
     try {
-      console.log("hii");
+      console.log("Running cron job to update price data");
+      const client = await clientPromise;
+      const markets = client.db("pascal").collection("markets");
+      // Extract the value of the publicKey field from each of the documents
+      const documents = await markets.find({}).toArray();
+      const pubKeys = documents.map((doc) => doc.publicKey);
+      console.log(pubKeys);
+
+      const program = getProgram();
+      console.log("Program", program);
+      for (const pubKey of pubKeys) {
+        const priceData = await getPriceData(program, pubKey);
+        const {
+          marketPriceSummary,
+          marketOutcomesSummary,
+          liquidityTotal,
+          matchedTotal,
+          totalUnmatchedOrders,
+        } = priceData;
+
+        await markets.updateOne(
+          { publicKey: pubKey },
+          {
+            $set: {
+              marketPriceSummary: marketPriceSummary,
+              liquidityTotal: liquidityTotal,
+              matchedTotal: matchedTotal,
+              totalUnmatchedOrders: totalUnmatchedOrders,
+              prices: marketPriceSummary,
+              outcomes: marketOutcomesSummary,
+            },
+          }
+        );
+      }
+      res.status(200).json({ success: true });
     } catch (err) {
       console.error("Cron job price data update error");
       res.status(500).json({ statusCode: 500, message: err.message });
